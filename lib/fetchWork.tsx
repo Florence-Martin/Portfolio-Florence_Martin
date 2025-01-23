92237fd513fbc8375c50b0ef228f58672e1b32ad
Florence Martin <florence.martin469@gmail.com>
2025-01-23 11:41:18 +0100

    refactor: host LinkedIn logos locally to avoid 403 errors

--- lib/fetchWork.tsx
+++ lib/fetchWork.tsx
@@ -1,8 +1,9 @@
+// /lib/fetchWork.ts
 export async function fetchWork() {
   return [
     {
-      image:
-        "https://media.licdn.com/dms/image/v2/C4D0BAQFctkw5r9W6_g/company-logo_100_100/company-logo_100_100/0/1630543297781/ingeli_logo?e=1737590400&v=beta&t=sj85Cyqs6NsXnTIx0-tSPXc8N9-cRjKnoudluBiTd80",
+      // On pointe vers les images stockées localement
+      image: "/assets/images/ingeli_logo.png",
       title: "Ingeli",
       role: "Développeuse fullstack",
       date: "2023 - 2024",
@@ -11,8 +12,7 @@ export async function fetchWork() {
       stack: ["C#", "MongoDB", "SCSS"],
     },
     {
-      image:
-        "https://media.licdn.com/dms/image/v2/D4E0BAQHsZadueOEMjQ/company-logo_100_100/company-logo_100_100/0/1707305304316/numericite_logo?e=1737590400&v=beta&t=4e9qVvDZkvV6dHQPqQiC_lLZZ0JTXe_pqx5CLE_2GAI",
+      image: "/assets/images/numericite_logo.jpeg",
       title: "NumeriCite",
       role: "Developpeuse web",
       date: "2021 - 2022",
